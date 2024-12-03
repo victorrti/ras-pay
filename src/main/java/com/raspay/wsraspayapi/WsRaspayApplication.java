eb215e8c11713d3a6bff109a5781ca8eb826f37b
victorrti <victorrodriguesti@gmail.com>
2024-12-03 10:55:55 -0300

    criando e configurando projeto raspay

---
+++ src/main/java/com/raspay/wsraspayapi/WsRaspayApplication.java
@@ -0,0 +1,13 @@
+package com.raspay.wsraspayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRaspayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsRaspayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raspay/wsraspayapi/WsRaspayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raspay.wsraspayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsRaspayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
